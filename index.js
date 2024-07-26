baf979bf4a884fdfaa96a9b336916291a639196e
gauthamkotian02 <gauthamkotian02@gmail.com>
2024-07-26 21:56:22 +0530

    7st commit july 26 2024

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 const express = require("express");
 const ConnectToMongo = require("./db");
-const cors = require("cors");
-
 ConnectToMongo();
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-
-const allowedOrigins = ["https://login-app-api.vercel.app"]; // Your frontend URL
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Allow cookies or auth headers
-  })
-);
+app.use(cors());
 
 const PORT = 4000;
-
 app.use("/customer", require("./Routes/customer_Routes"));
 
-// Handle preflight requests
-app.options("*", cors());
+//uploads
+// app.use("/uploads/customer", express.static("./Uploads/customer"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
