32d7208d279b573bc613e0257acd9393c7cf5f6c
gauthamkotian02 <gauthamkotian02@gmail.com>
2024-07-27 09:37:47 +0530

    27 july 2024

--- Routes/customer_Routes.js
+++ Routes/customer_Routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
 const {
   Register,
   Login,
@@ -8,18 +7,6 @@ const {
   verifyOtp,
   customer,
 } = require("../controller/Customer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/customer/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
 
 router.post("/register", Register);
 router.post("/login", Login);
