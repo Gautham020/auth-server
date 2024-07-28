5b795ae3e74bb7c19e36f7fe548b7861e25bf2cf
Gautham020 <141896073+Gautham020@users.noreply.github.com>
2024-07-28 20:29:49 +0530

    Update index.js for cors credential

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://login-app-api.vercel.app"],
+    method: ["POST", "GET"],
+    credentials: true,
+  }));
 const PORT = process.env.PORT || 4000;
 app.use("/customer", require("./Routes/customer_Routes"));
 
